bf949f4f6312f4acd13f42de84e455dd05398cd5
grasielarosa <grasielarosaa@gmail.com>
2021-04-17 18:22:11 +0200

    ejercicios 1 a 10

---
+++ script.js
@@ -0,0 +1,24 @@
+//EJERCICIO 1: Tienen misma longitud
+// const tienenMismaLongitud=(a,b)=>{
+//     return (a.length===b.length)
+//   }
+//EJERCICIO 2: Es el último caracter
+// const esElUltimoCaracter= (palabra,caracter)=>{
+//     return (palabra.slice(-1)===caracter)
+//   }
+//EJERCICIO 3
+//EJERCICIO 4
+//EJERCICIO 5: Contar palabras
+// const contarPalabras=(str)=>{
+//     return (str.split(" ").length)
+//   }
+//EJERCICIO 6: Burlarse
+// const burlarse = (str) =>{
+//     return str.replace(/[aeiou]/g, 'i');
+// }
+//EJERCICIO 7: Es fraccion mayor a uno
+
+//EJERCICIO 8: Capitalizar
+// const capitalizar=(str)=>{
+//     return (str.charAt(0).toUpperCase() + str.slice(1))
+//   }
